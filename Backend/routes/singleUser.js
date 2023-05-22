f8b19ed80c97797281a39e631ed42aa2a98321b7
pavanagula-19 <Pavanagulla19@gmail.com>
2023-05-23 00:16:49 +0530

    yes

--- Backend/routes/singleUser.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { updatelist,singleuser, dellist } = require("../controller/singleUser");
-const router = express.Router();
-
-router.put("/update/:id",updatelist)
-router.get("/singleuser/:id",singleuser)
-router.put("/delete-list/:id",dellist)
-module.exports = router;
